651068cf1fa731a5b7d14053fd881c9425cd2426
Marioneq 4958 <mq4958@gmail.com>
2023-03-18 18:25:58 +0100

    Add meetings

---
+++ sdk_python/hebe/data/meeting.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, root_validator
+
+from sdk_python.hebe.api import API, FilterListType
+from sdk_python.hebe.data.pupil import Period, Pupil
+from sdk_python.hebe.error import InvalidResponseEnvelopeTypeException
+
+
+class Meeting(BaseModel):
+    id: int = Field(alias="Id")
+    subject: str = Field(alias="Why")
+    date: datetime = Field(alias="When")
+    place: str = Field(alias="Where")
+    agenda: str = Field(alias="Agenda")
+    online: Optional[str] = Field(alias="Online")
+
+    @root_validator(pre=True)
+    def root_validator(cls, values):
+        values["When"] = datetime.fromtimestamp(values["When"]["Timestamp"] / 1000)
+        return values
+
+    @staticmethod
+    async def get_by_pupil(
+        api: API, pupil: Pupil, period: Period, **kwargs
+    ) -> list["Meeting"]:
+        envelope, envelope_type = await api.get(
+            entity="meetings",
+            filter_list_type=FilterListType.BY_PUPIL,
+            rest_url=pupil.unit.rest_url,
+            pupil_id=pupil.id,
+            period_id=period.id,
+            from_=period.start,
+            **kwargs
+        )
+        if envelope_type != "IEnumerable`1":
+            raise InvalidResponseEnvelopeTypeException()
+        return [Meeting.parse_obj(meeting) for meeting in envelope]
