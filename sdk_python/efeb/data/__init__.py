1e895c0b6e57481e995f09297983ff983b8d88fc
Marioneq 4958 <marioneq4958@gmail.com>
2024-03-19 11:22:29 +0100

    Remove efeb module

--- sdk_python/efeb/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .exam import *
-from .meeting import *
-from .note import *
-from .student import *
--- sdk_python/efeb/models/student/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cache import *
-from .exam import *
-from .meeting import *
-from .note import *
-from .register import *
-from .school import *
-from .student import *
